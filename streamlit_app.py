9613048f1fa139fe9a80be2d44dbf76535a7e30e
cparikshit <pchada@apexsystems.com>
2025-01-28 12:07:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
-
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
